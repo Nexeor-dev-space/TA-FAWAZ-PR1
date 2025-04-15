cc5510f2b61928344b598b03e4c2302f5f91b637
Fawaz-Khan-R <115716567+Fawaz-Khan-R@users.noreply.github.com>
2025-04-15 10:59:43 +0530

    feat: #1 Added Heading and p tags

---
+++ furnishop/components/Hero.js
@@ -0,0 +1,19 @@
+import styles from '../styles/Hero.module.css';
+
+const Hero = () => {
+  return (
+    <div className={styles.hero}>
+      <div className={styles.content}>
+        <h1 className={styles.heading}>
+          Creative Home Simplify Your <br />
+          Furniture
+        </h1>
+        <p className={styles.subtext}>
+          Do i have consent to record this meeting gain location, root-and-branch, review, nor game plan who's the goto
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Hero; 
